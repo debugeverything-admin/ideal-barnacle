fb6e96f80eab2fe9f28c582e5d2c1a002b939b7f
debugeverything-admin <95109673+debugeverything-admin@users.noreply.github.com>
2022-08-20 12:48:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(12347777);
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
